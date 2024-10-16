64049ba90f0c35ef96c7601fd11ed4a16601f1db
Sandeep Kumar <58764439+sandkum5@users.noreply.github.com>
2024-10-16 14:46:39 -0700

    Update reserveWwpn.py

--- intersight/python/reserveWwpn.py
+++ intersight/python/reserveWwpn.py
@@ -1,7 +1,23 @@
 #!/usr/bin/env python3
 """
-    Reserve WWPNs and assign to a profile vHBAs
-    Uses Intersight OAuth 2.0 Authentication
+    - Reserve WWPNs and assign to a profile vHBAs
+    - Uses Intersight OAuth 2.0 Authentication
+    - Ensure WWPN's are not in use.
+
+    Pre-requisites:
+    - Organization
+    - WWPN Pool
+    - Server Profiles with no SAN Connectivity Policy
+    - SAN Connectivity Policy
+
+    Sample Output:
+    ❯ ./demo_v2.py
+    Server Profile: demo1 WWPNs associated successfully!
+      vhba0: 20:00:00:25:B5:AA:AA:31
+      vhba1: 20:00:00:25:B5:AA:AA:32
+    Server Profile: demo2 WWPNs associated successfully!
+      vhba0: 20:00:00:25:B5:AA:AA:21
+      vhba1: 20:00:00:25:B5:AA:AA:22
 """
 import os
 import sys
@@ -22,7 +38,6 @@ def get_token(client_id, client_secret):
     if response.status_code != 200:
         print("Failed to obtain token from the OAuth 2.0 server", file=sys.stderr)
         sys.exit(1)
-    print("Successfuly obtained a new token")
     token_json = response.json()
     return token_json["access_token"]
 
@@ -48,106 +63,66 @@ def post_api_data(token, client_id, client_secret, api_url, post_data):
     else:
         return response.json()
 
-if __name__ == '__main__':
-    # Variables
-    client_id           = os.getenv("ClientId")
-    client_secret       = os.getenv("ClientSecret")
-    org_name            = "default"
-    fcpool_name         = "demoxwwpn"
-    sp_name             = "demo1"
-    sanconn_policy_name = "demox"
-    vhba0_name          = "vhba0"
-    vhba1_name          = "vhba1"
-    vhba0_wwpn          = "20:00:00:25:B5:AA:AA:21"
-    vhba1_wwpn          = "20:00:00:25:B5:AA:AA:22"
-
-    # URLs    
-    org_url             = f"https://intersight.com/api/v1/organization/Organizations?$filter=Name eq '{org_name}'"
-    fcpool_url          = f"https://intersight.com/api/v1/fcpool/Pools?$filter=Name eq '{fcpool_name}'"
-    sp_url              = f"https://intersight.com/api/v1/server/Profiles?$filter=Name eq '{sp_name}'"
-    san_conn_url        = f"https://intersight.com/api/v1/vnic/SanConnectivityPolicies?$filter=Name eq '{sanconn_policy_name}'"
-
-    # Get oAuth Token
-    token = get_token(client_id, client_secret)
-
-    # Get API Endpoint Data
-    # Get Org, WWPN Pool and Server Profile Info
-    org_data = get_api_data(token, client_id, client_secret, org_url)
-    fcpool_data = get_api_data(token, client_id, client_secret, fcpool_url)
-    sp_data = get_api_data(token, client_id, client_secret, sp_url)
-    san_conn_policy_data = get_api_data(token, client_id, client_secret, san_conn_url)
-
-    # Create WWPN Reservations
+def create_wwpn_reservations(org_moid, fcpool_moid, wwpn):
+    """
+        Create WWPN Reservations
+    """
     res_url = f"https://intersight.com/api/v1/fcpool/Reservations"
-    vhba0_data = {
-        "AllocationType": "dynamic",
-        "IdPurpose": "WWPN",
-        "Identity": f"{vhba0_wwpn}",
-        "Organization": {
-            "ClassId": "mo.MoRef",
-            "Moid": f"{org_data['Moid']}",
-            "ObjectType": "organization.Organization",
-            "link": f"https://intersight.com/api/v1/organization/Organizations/{org_data['Moid']}"
-        },
-        "Pool": {
-            "ClassId": "mo.MoRef",
-            "Moid": f"{fcpool_data['Moid']}",
-            "ObjectType": "fcpool.Pool",
-            "link": f"https://intersight.com/api/v1/fcpool/Pools/{fcpool_data['Moid']}"    
-        }
-    }
-    vhba1_data = {
-        "AllocationType": "dynamic",
-        "IdPurpose": "WWPN",
-        "Identity": f"{vhba1_wwpn}",
-        "Organization": {
-            "ClassId": "mo.MoRef",
-            "Moid": f"{org_data['Moid']}",
-            "ObjectType": "organization.Organization",
-            "link": f"https://intersight.com/api/v1/organization/Organizations/{org_data['Moid']}"
-        },
-        "Pool": {
-            "ClassId": "mo.MoRef",
-            "Moid": f"{fcpool_data['Moid']}",
-            "ObjectType": "fcpool.Pool",
-            "link": f"https://intersight.com/api/v1/fcpool/Pools/{fcpool_data['Moid']}"    
-        }
-    }
-    vhba0_json = json.dumps(vhba0_data)
-    vhba1_json = json.dumps(vhba1_data)
-    vhba0_res_data = post_api_data(token, client_id, client_secret, res_url, vhba0_json)
-    vhba1_res_data = post_api_data(token, client_id, client_secret, res_url, vhba1_json)
-    print("WWPN Reservations Created Successfully!")
-    # Add Reservation References to the Profile
-    rr_url = f"https://intersight.com/api/v1/server/Profiles/{sp_data['Moid']}"
-    rr_vhba_data = {
-        "ReservationReferences": [
-            {
-                "ClassId": "fcpool.ReservationReference",
-                "ConsumerName": f"{vhba0_name}",
-                "ConsumerType": "Vhba",
-                "ObjectType": "fcpool.ReservationReference",
-                "ReservationMoid": f"{vhba0_res_data['Moid']}"
+    wwpn_res_moids = {}
+    for key,value in wwpn.items():
+        vhba_data = {
+            "AllocationType": "dynamic",
+            "IdPurpose": "WWPN",
+            "Identity": f"{value}",
+            "Organization": {
+                "ClassId": "mo.MoRef",
+                "Moid": f"{org_moid}",
+                "ObjectType": "organization.Organization",
+                "link": f"https://intersight.com/api/v1/organization/Organizations/{org_moid}"
             },
-            {
-                "ClassId": "fcpool.ReservationReference",
-                "ConsumerName": f"{vhba1_name}",
-                "ConsumerType": "Vhba",
-                "ObjectType": "fcpool.ReservationReference",
-                "ReservationMoid": f"{vhba1_res_data['Moid']}"
+            "Pool": {
+                "ClassId": "mo.MoRef",
+                "Moid": f"{fcpool_moid}",
+                "ObjectType": "fcpool.Pool",
+                "link": f"https://intersight.com/api/v1/fcpool/Pools/{fcpool_moid}"    
             }
-        ]
-    }
+        }
+
+        vhba_json = json.dumps(vhba_data)
+        vhba_res_data = post_api_data(token, client_id, client_secret, res_url, vhba_json)
+        wwpn_res_moids[key] = vhba_res_data['Moid']
+    return wwpn_res_moids
+
+def add_res_ref_to_profile(sp_moid, wwpn_res_moids):
+    """
+        Add Reservation References to the Profile
+    """
+    rr_url = f"https://intersight.com/api/v1/server/Profiles/{sp_moid}"
+    rr_vhba_data = {"ReservationReferences": []}
+    for vhba_name,wwpn_res_moid in wwpn_res_moids.items(): 
+        res_data = {
+            "ClassId": "fcpool.ReservationReference",
+            "ConsumerName": f"{vhba_name}",
+            "ConsumerType": "Vhba",
+            "ObjectType": "fcpool.ReservationReference",
+            "ReservationMoid": f"{wwpn_res_moid}"
+        }
+        rr_vhba_data["ReservationReferences"].append(res_data)
+
     rr_vhba_json = json.dumps(rr_vhba_data)
     vhba_rr_data = post_api_data(token, client_id, client_secret, rr_url, rr_vhba_json)
-    print("WWPN Reservations Added to the Profile Successfully!")
-    # Associate SAN Connectivity Policy to the Profile
-    sp_post_url = f"https://intersight.com/api/v1/server/Profiles/{sp_data['Moid']}"
+    return vhba_rr_data
+
+def add_sanconn_policy_to_profile(sp_moid, sanpolicy_moid, sp_data):
+    """
+        Associate SAN Connectivity Policy to the Profile
+    """
+    sp_post_url = f"https://intersight.com/api/v1/server/Profiles/{sp_moid}"
     san_policy_data = {
         "ClassId": "mo.MoRef",
-        "Moid": f"{san_conn_policy_data['Moid']}",
+        "Moid": f"{sanpolicy_moid}",
         "ObjectType": "vnic.SanConnectivityPolicy",
-        "link": f"https://intersight.com/api/v1/vnic/SanConnectivityPolicies/{san_conn_policy_data['Moid']}"
+        "link": f"https://intersight.com/api/v1/vnic/SanConnectivityPolicies/{sanpolicy_moid}"
     }
 
     post_data = { "PolicyBucket": sp_data['PolicyBucket']}  
@@ -155,4 +130,60 @@ if __name__ == '__main__':
     policy_bucket_json = json.dumps(post_data)
 
     updated_sp_data = post_api_data(token, client_id, client_secret, sp_post_url, policy_bucket_json)
-    print("SAN Connectivity Policy assigned to the profile Successfully!")
+    return updated_sp_data
+
+
+if __name__ == '__main__':
+    # Variables
+    client_id     = os.getenv("ClientId")
+    client_secret = os.getenv("ClientSecret")
+
+    with open('py_input.json') as f:
+        data = json.load(f)
+
+    for server in data:
+        org_name            = server['org_name']
+        fcpool_name         = server['fcpool_name']
+        sp_name             = server['sp_name']
+        sanconn_policy_name = server['sanconn_policy_name']
+        vhba0_name          = server['vhba0_name']
+        vhba1_name          = server['vhba1_name']
+        wwpn = {
+            "vhba0": server['vhba0_wwpn'],
+            "vhba1": server['vhba1_wwpn']
+        }
+
+        # URLs    
+        org_url             = f"https://intersight.com/api/v1/organization/Organizations?$filter=Name eq '{org_name}'"
+        fcpool_url          = f"https://intersight.com/api/v1/fcpool/Pools?$filter=Name eq '{fcpool_name}'"
+        sp_url              = f"https://intersight.com/api/v1/server/Profiles?$filter=Name eq '{sp_name}'"
+        san_conn_url        = f"https://intersight.com/api/v1/vnic/SanConnectivityPolicies?$filter=Name eq '{sanconn_policy_name}'"
+
+        # Get oAuth Token
+        token = get_token(client_id, client_secret)
+
+        # Get API Endpoint Data
+        # Get Org, WWPN Pool and Server Profile Info
+        org_data       = get_api_data(token, client_id, client_secret, org_url)
+        sp_data        = get_api_data(token, client_id, client_secret, sp_url)
+        fcpool_data    = get_api_data(token, client_id, client_secret, fcpool_url)
+        sanpolicy_data = get_api_data(token, client_id, client_secret, san_conn_url)
+
+        # Get Moids
+        org_moid = org_data['Moid']
+        sp_moid = sp_data['Moid']
+        fcpool_moid = fcpool_data['Moid']
+        sanpolicy_moid = sanpolicy_data['Moid']
+
+        # Create WWPN Reservations
+        wwpn_res_moids = create_wwpn_reservations(org_moid, fcpool_moid, wwpn)
+
+        # Add Reservation References to the Profile
+        vhba_rr_data = add_res_ref_to_profile(sp_moid, wwpn_res_moids)
+
+        # Associate SAN Connectivity Policy to the Profile
+        updated_sp_data = add_sanconn_policy_to_profile(sp_moid, sanpolicy_moid, sp_data)
+
+        print(f"Server Profile: {sp_name} WWPNs associated successfully!")
+        for key,value in wwpn.items():
+            print(f"  {key}: {value}")
